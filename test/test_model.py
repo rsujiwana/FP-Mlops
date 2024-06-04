bd4e93d7833d9793c31a2ebe64d3c8964e350b0a
rsujiwana <113880876+rsujiwana@users.noreply.github.com>
2024-06-04 16:50:56 +0700

    Update test_model.py

--- test/test_model.py
+++ test/test_model.py
@@ -1,7 +1,7 @@
 import pytest
-from app.utils import your_labeling_function
+from app.utils import labeling_function
 
 def test_labeling_function():
     input_data = "This is a sample input text."
     expected_output = {"label": "Expected label"}  # Sesuaikan dengan output yang diharapkan
-    assert your_labeling_function(input_data) == expected_output
+    assert labeling_function(input_data) == expected_output
