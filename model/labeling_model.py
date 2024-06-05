80783837de5ab0b4256a9c05f0d0724903fdd430
rsujiwana <113880876+rsujiwana@users.noreply.github.com>
2024-06-05 10:29:19 +0700

    Update labeling_model.py

--- model/labeling_model.py
+++ model/labeling_model.py
@@ -1,29 +1,18 @@
-from pyabsa import ABSADatasetList, APCModelList, available_checkpoints
-from pyabsa import AspectTermExtraction as ATEPC
-from pyabsa import ATEPCConfigManager
+from pyabsa import AspectTermExtraction as ATEPC, available_checkpoints
 
-config = ATEPCConfigManager.get_atepc_config_english()
-dataset = ABSADatasetList.SemEval
+# you can view all available checkpoints by calling available_checkpoints()
+checkpoint_map = available_checkpoints()
 
-# checkpoint_map = available_checkpoints(
-#     TaskCodeOption.Aspect_Polarity_Classification, show_ckpts=True
-# )
-
-aspect_extractor = ATEPC.ATEPCTrainer(
-    config=config,
-    from_checkpoint='checkpoints.json'   #checkpoint_map,   # not necessary for most situations
-    dataset: dataset,  # Corrected line
-    checkpoint_save_mode=1,
-    auto_device=True,
-    load_aug=False,
-).load_trained_model()
+aspect_extractor = ATEPC.AspectExtractor('multilingual',
+                                         auto_device=True,  # False means load model on CPU
+                                         cal_perplexity=True,
+                                         )
 
 def label_text(text):
     # Implementasi pelabelan dengan model yang sudah dilatih
-    result = aspect_extractor.batch_predict(
-        target_file=atepc_examples,  #
-        save_result=True,
-        print_result=True,  # print the result
-        pred_sentiment=True,  # Predict the sentiment of extracted aspect terms
-)
+    result = aspect_extractor.predict([text],
+                         save_result=True,
+                         print_result=True,  # print the result
+                         ignore_error=True,  # ignore the error when the model cannot predict the input
+                         )
     return result
