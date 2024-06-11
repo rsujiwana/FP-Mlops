788d6467347edfd3dba742662caf43b042785f1b
rsujiwana <113880876+rsujiwana@users.noreply.github.com>
2024-06-11 19:43:08 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,11 @@
 
 set -o errexit
 set -o nounset
-set -o pipefail
 
 
 /usr/local/bin/uwsgi --http 0.0.0.0:5000 \
     --plugins python3 \
-    --wsgi wsgi:run
+    --wsgi run:app
 
 echo ""
 echo "$@"
